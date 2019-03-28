1a62f59589bc209ccdbe52c3b188eeb7a1030905
Alexander Craggs <github@femto.host>
2019-03-28 20:08:46 +0000

    Functioning consumer using lib

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const expressSession = require('express-session')
 const MongoStore = require('connect-mongo')(expressSession)
 const MongoClient = require('mongodb').MongoClient
 const cookieParser = require('cookie-parser')
-const fetch = require('node-fetch')
 const express = require('express')
 const config = require('@femto-apps/config')
+const authenticationConsumer = require('../lib-authentication-consumer')
 
 ;(async () => {
     const app = express()
@@ -15,7 +15,7 @@ const config = require('@femto-apps/config')
     app.set('view engine', 'pug')
 
     app.use(express.static('public'))
-    app.use(cookieParser(config.get('cookie.secret'))
+    app.use(cookieParser(config.get('cookie.secret')))
     app.use(expressSession({
         secret: config.get('session.secret'),
         resave: false,
@@ -26,42 +26,18 @@ const config = require('@femto-apps/config')
             maxAge: config.get('cookie.maxAge')
         }
     }))
-    app.use((req, res, next) => {
-        if (req.session.token) {
-            fetch(`${config.get('token')}${req.session.token}`)
-                .then(resp => resp.json())
-                .then(resp => {
-                    req.session.user = resp
-                    next()
-                })
-        } else {
-            next()
-        }
-    })
-    app.use((req, res, next) => {
-        req.user = req.session.user
-        next()
-    })
+    app.use(authenticationConsumer({
+        tokenService: { endpoint: 'http://localhost:4500' },
+        authenticationProvider: { endpoint: 'http://localhost:3001', consumerId: 100 },
+        authenticationConsumer: { endpoint: 'http://localhost:3002' }
+    }))
+
+  //   tokenService: { endpoint },
+  //   authenticationProvider: { endpoint, id, consumerId },
+  //   authenticationConsumer: { endpoint }
 
     app.get('/', (req, res) => {
-        console.log('hello', req.user)
         res.render('home', { user: req.user } )
-        console.log(req.session)
-    })
-    app.get('/login', (req, res) => {
-        console.log('redirecting')
-        res.redirect(config.get('provider.login'))
-        console.log('redirected')
-    })
-    app.get('/login_callback', (req, res) => {
-        const { token } = req.query
-        req.session.token = token
-        res.redirect('/')
-    })
-    app.get('/logout', (req, res) => {
-        req.session.user = undefined
-        req.session.token = undefined
-        res.redirect(config.get('provider.logout'))
     })
 
     app.listen(port, () => console.log(`Example app listening on port ${port}!`))
